8c4131f73ab54c648d58823f05d0c8379a5bfb44
masakifujiwara <fmasaki2020@gamil.com>
2023-08-24 17:53:27 +0900

    setup

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "docker container run --network host --gpus all -it --name my-noetic -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged only-noetic-cartographer"
+eval "docker container run --network host --gpus all -it --name my-noetic-cartographer -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged only-noetic-cartographer"
