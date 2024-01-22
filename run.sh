df4283507db3d2d04c615e1b60098849f12a6ed8
masakifujiwara1 <72371743+masakifujiwara1@users.noreply.github.com>
2024-01-22 14:58:43 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eval "docker container run --network host --gpus all --rm -it --name my-noetic -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged masakifujiwara1/ros1:noetic"
+eval "docker container run --network host -it --name my-noetic -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged -v /dev:/dev masakifujiwara1/ros1:noetic"
