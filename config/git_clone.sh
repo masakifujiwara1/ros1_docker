4fc15322abc10e17c996de5e9fb232427c1b1480
MasakiFujiwara <fmasaki2020@gmail.com>
2024-08-05 05:46:26 +0900

    Delete file

--- config/git_clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# sample
-# eval "git clone https://github.com/openspur/yp-spur"
-# eval "git clone -b noetic-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git"
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-eval "docker container run --network host --gpus all -it --name my-noetic-cartographer -e DISPLAY=$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $PWD/docker_share:/home/host_files --privileged only-noetic-cartographer"
